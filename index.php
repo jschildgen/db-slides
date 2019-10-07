46ccf54e9a4db1858ab537fa62172dc0d893e886
Johannes Schildgen <johannes@bt-nac-l205.nac.uni-bayreuth.de>
2019-10-07 15:14:11 +0200

    Index file that lists all slideshows and exercise sheets

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Slides</title>
+    <link href="./lib/fontawesome.all.min.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>Slides</h1>
+<?php
+$files = glob('./*.{html}', GLOB_BRACE);
+foreach($files as $file) {
+  echo "<a href=\"$file\">$file</a> <a href=\"$file?print-pdf&showNotes=true\"><i class=\"fas fa-print\"></i></a><br>";
+}
+?>
+<hr>
+<h1>Exercise Sheets</h1>
+<?php
+$files = glob('./exercises/*.{html}', GLOB_BRACE);
+foreach($files as $file) {
+  echo "<a href=\"$file\">$file</a><br>";
+}
+?>
+</body>
+</html>

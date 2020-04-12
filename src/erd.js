420e3ee25925713c0ea9f8aa73ceafbdd6e57678
Johannes Schildgen <schildgen@cs.uni-kl.de>
2020-04-12 10:01:04 +0200

    Fix in erd.js for recursive relationships

--- src/erd.js
+++ src/erd.js
@@ -384,6 +384,121 @@ class Relationship extends erd.Relationship {
 
 // Helpers
 
+/** https://resources.jointjs.com/tutorial/multiple-links-between-elements */
+function adjustVertices(graph, cell) {
+
+    // if `cell` is a view, find its model
+    cell = cell.model || cell;
+
+    if (cell instanceof joint.dia.Element) {
+        // `cell` is an element
+
+        _.chain(graph.getConnectedLinks(cell))
+            .groupBy(function(link) {
+
+                // the key of the group is the model id of the link's source or target
+                // cell id is omitted
+                return _.omit([link.source().id, link.target().id], cell.id)[0];
+            })
+            .each(function(group, key) {
+
+                // if the member of the group has both source and target model
+                // then adjust vertices
+                if (key !== 'undefined') adjustVertices(graph, _.first(group));
+            })
+            .value();
+
+        return;
+    }
+
+    // `cell` is a link
+    // get its source and target model IDs
+    var sourceId = cell.get('source').id || cell.previous('source').id;
+    var targetId = cell.get('target').id || cell.previous('target').id;
+
+    // if one of the ends is not a model
+    // (if the link is pinned to paper at a point)
+    // the link is interpreted as having no siblings
+    if (!sourceId || !targetId) return;
+
+    // identify link siblings
+    var siblings = _.filter(graph.getLinks(), function(sibling) {
+
+        var siblingSourceId = sibling.source().id;
+        var siblingTargetId = sibling.target().id;
+
+        // if source and target are the same
+        // or if source and target are reversed
+        return ((siblingSourceId === sourceId) && (siblingTargetId === targetId))
+            || ((siblingSourceId === targetId) && (siblingTargetId === sourceId));
+    });
+
+    var numSiblings = siblings.length;
+    switch (numSiblings) {
+
+        case 0: {
+            // the link has no siblings
+            break;
+
+        } case 1: {
+            // there is only one link
+            // no vertices needed
+            cell.unset('vertices');
+            break;
+
+        } default: {
+            // there are multiple siblings
+            // we need to create vertices
+
+            // find the middle point of the link
+            var sourceCenter = graph.getCell(sourceId).getBBox().center();
+            var targetCenter = graph.getCell(targetId).getBBox().center();
+            var midPoint = g.Line(sourceCenter, targetCenter).midpoint();
+
+            // find the angle of the link
+            var theta = sourceCenter.theta(targetCenter);
+
+            // constant
+            // the maximum distance between two sibling links
+            var GAP = 20;
+
+            _.each(siblings, function(sibling, index) {
+
+                // we want offset values to be calculated as 0, 20, 20, 40, 40, 60, 60 ...
+                var offset = GAP * Math.ceil(index / 2);
+
+                // place the vertices at points which are `offset` pixels perpendicularly away
+                // from the first link
+                //
+                // as index goes up, alternate left and right
+                //
+                //  ^  odd indices
+                //  |
+                //  |---->  index 0 sibling - centerline (between source and target centers)
+                //  |
+                //  v  even indices
+                var sign = ((index % 2) ? 1 : -1);
+
+                // to assure symmetry, if there is an even number of siblings
+                // shift all vertices leftward perpendicularly away from the centerline
+                if ((numSiblings % 2) === 0) {
+                    offset -= ((GAP / 2) * sign);
+                }
+
+                // make reverse links count the same as non-reverse
+                var reverse = ((theta < 180) ? 1 : -1);
+
+                // we found the vertex
+                var angle = g.toRad(theta + (sign * reverse * 90));
+                var vertex = g.Point.fromPolar(offset, angle, midPoint);
+
+                // replace vertices array with `vertex`
+                sibling.vertices([vertex]);
+            });
+        }
+    }
+}
+
 var createLink = function(elm1, elm2, graph) {
 
     var myLink = new erd.Line({
@@ -483,6 +598,15 @@ function createERD(er, erdDiv, width, height) {
 
         highlighter.remove();
     });
+
+    // bind `graph` to the `adjustVertices` function
+    var adjustGraphVertices = _.partial(adjustVertices, graph);
+
+    // adjust vertices when a cell is removed or its source/target was changed
+    graph.on('add remove change:source change:target', adjustGraphVertices);
+
+    // adjust vertices when the user stops interacting with an element
+    paper.on('cell:pointerup', adjustGraphVertices);
     
     for(e in entities) {
         /* create entity */

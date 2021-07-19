89b727b86b928ea701c893c063fcf67dcff66437
jschildgen <jschildgen@users.noreply.github.com>
2021-07-19 09:59:23 +0000

    deploy: f43c0bd1039fac660fd53230ba6e75483d95e442

--- src/exam.js
+++ src/exam.js
@@ -1,8 +1,25 @@
+function txt(v) {
+  var lang = Array();
+  lang["de"] = Array();
+  lang["de"]["Exercise"] = "Aufgabe";
+  lang["de"]["Max"] = "Max";
+  lang["de"]["P"] = "P";
+  lang["de"]["Points"] = "Punkte";
+  lang["de"]["Total"] = "Gesamt";
+  lang["de"]["Grade"] = "Note";
+  
+  var code = document.getElementsByTagName('html')[0].attributes["lang"].value;
+  if(lang[code] == undefined || lang[code][v] == undefined) {
+    return v;
+  }
+  return lang[code][v];
+}
+
 (function() {
 
-  var points_tbl_head = '<thead><tr><th>Aufgabe:</th>'; //<th style="width: 15mm;">1</th><th style="width: 15mm;">2</th><th style="width: 15mm;">3</th><th style="width: 15mm;">4</th><th style="width: 15mm;">5</th><th style="width: 15mm;">6</th><th style="width: 15mm;">7</th><th style="width: 25mm;">Gesamt</th><th style="width: 25mm;">Note</th></tr></thead>
-  var points_tbl_body1 = '<tbody><tr><th>Max:</th>'; //<td>15</td><td>10</td><td>16</td><td>8</td><td>8</td><td>18</td><td>5</td><td>90</td><td></td></tr>
-  var points_tbl_body2 = '<tr><th>Punkte:</th>'; //<td><td></td><td></td><td></td></td><td></td><td></td><td></td><td></td><td></td></tr>
+  var points_tbl_head = '<thead><tr><th>'+txt('Exercise')+':</th>'; //<th style="width: 15mm;">1</th><th style="width: 15mm;">2</th><th style="width: 15mm;">3</th><th style="width: 15mm;">4</th><th style="width: 15mm;">5</th><th style="width: 15mm;">6</th><th style="width: 15mm;">7</th><th style="width: 25mm;">Gesamt</th><th style="width: 25mm;">Note</th></tr></thead>
+  var points_tbl_body1 = '<tbody><tr><th>'+txt('Max')+':</th>'; //<td>15</td><td>10</td><td>16</td><td>8</td><td>8</td><td>18</td><td>5</td><td>90</td><td></td></tr>
+  var points_tbl_body2 = '<tr><th>'+txt('Points')+':</th>'; //<td><td></td><td></td><td></td></td><td></td><td></td><td></td><td></td><td></td></tr>
         //</tbody>
 
   var exercise_no = 0;
@@ -18,13 +35,13 @@
           exercise_points += points;
         });
       total_points += exercise_points;
-      e.querySelector("h1").innerHTML = "Aufgabe "+exercise_no+": "+e.querySelector("h1").innerHTML+" ("+exercise_points+"P)";
+      e.querySelector("h1").innerHTML = txt('Exercise')+" "+exercise_no+": "+e.querySelector("h1").innerHTML+" ("+exercise_points+txt("P")+")";
       points_tbl_head += '<th style="width: 15mm;">'+exercise_no+'</th>';
       points_tbl_body1 += '<td>'+exercise_points+'</td>';
       points_tbl_body2 += '<td></td>';
     });
 
-    points_tbl_head += '<th style="width: 25mm;">Gesamt</th><th style="width: 25mm;">Note</th></tr></thead>';
+    points_tbl_head += '<th style="width: 25mm;">'+txt('Total')+'</th><th style="width: 25mm;">'+txt('Grade')+'</th></tr></thead>';
     points_tbl_body1 += '<td>'+total_points+'</td><td></td></tr>';
     points_tbl_body2 += '<td></td><td></td></tr></tbody>';
 
